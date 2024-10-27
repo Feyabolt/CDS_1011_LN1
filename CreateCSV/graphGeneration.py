fa5f8a6c730ce8c2d6382e6c03d357ad587453a4
Feyabolt <nadler.fabian@bluewin.ch>
2024-10-27 18:02:12 +0100

    Generation of new Figures, graphs now have Y-Axis Grid and starts at 19. Feb and ends at 07. Jun

--- CreateCSV/graphGeneration.py
+++ CreateCSV/graphGeneration.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 
 #df.drop(["Total_Sleep", "Date_Time", "Deep_Sleep", "Light_Sleep", "REM_Time", "Awake", "Intensity", "Steps", "Stress", "Elevation_Up", "Elevation_Down"], axis = 1, inplace=True)
 # Download and prepare the data
-data_root = "C:/Users/nadle/Documents/GarminData/SleepExport/Schlafdaten.csv"
+data_root = "GarminData/DataExport/Schlafdaten.csv"
 df = pd.read_csv(data_root)
 
 totalSleepCol = 2
@@ -34,34 +34,40 @@ for index in reversed(range(len(df))):
             df.iat[index,RemCol] = round(df.iat[index, RemCol]/df.iat[index, totalSleepCol]*100,2)
             
 
-
+indexStartTime = pd.Timestamp('2023-02-19')
+indexEndTime = pd.Timestamp('2023-06-07')
 
 df.index = pd.to_datetime(df["Date"])
 
 #Plotting the first table
-firstTable = [df[["REM_Time", "Deep_Sleep"]], df[["Intensity"]]]
+firstTable = [df[["Total_Sleep"]], df[["Stress"]]]
 
-firstTable[0].plot(ylim=(0,100),ylabel="Schlafdaten")
-firstTable[1].plot(secondary_y=True, ax=plt.gca(), ylabel="Intensität")
+firstTable[0].plot(ylim=(150,700),ylabel="Sleep Time (min)",xlim=(indexStartTime,indexEndTime)).grid(axis='y')
+firstTable[1].plot(secondary_y=True, ax=plt.gca(), ylabel="Stress")
 
 #Plotting the second table
-secondTable = [df[["Light_Sleep", "Awake"]], df[["Intensity"]]]
+secondTable = [df[["Light_Sleep"]], df[["Stress"]]]
 
-secondTable[0].plot(ylim=(0,100),ylabel="Schlafdaten")
-secondTable[1].plot(secondary_y=True, ax=plt.gca(), ylabel="Intensität")
+secondTable[0].plot(ylim=(0,100),ylabel="Light sleep (%)",xlim=(indexStartTime,indexEndTime)).grid(axis='y')
+secondTable[1].plot(secondary_y=True, ax=plt.gca(), ylabel="Stress")
 
 #Plotting the third table
-thirdTable = [df[["Total_Sleep"]], df[["Stress"]]]
+thirdTable = [df[["Deep_Sleep"]], df[["Stress"]]]
 
-thirdTable[0].plot(ylabel="Schlafdaten")
+thirdTable[0].plot(ylabel="Deep Sleep (%)",xlim=(indexStartTime,indexEndTime)).grid(axis='y')
 thirdTable[1].plot(secondary_y=True, ax=plt.gca(), ylabel="Stress")
 
 #Plotting the fourth table
-fourthTable = [df[["Light_Sleep", "Awake"]], df[["Stress"]]]
+fourthTable = [df[["Light_Sleep"]], df[["Intensity"]]]
 
-fourthTable[0].plot(ylim=(0,100),ylabel="Schlafdaten")
-fourthTable[1].plot(secondary_y=True, ax=plt.gca(), ylabel="Stress")
+fourthTable[0].plot(ylim=(0,100),ylabel="Light Sleep (%)",xlim=(indexStartTime,indexEndTime)).grid(axis='y')
+fourthTable[1].plot(secondary_y=True, ax=plt.gca(), ylabel="Intensity")
 
+#Plotting the fifth table
+fifthTable = [df[["Deep_Sleep"]], df[["Intensity"]]]
+
+fifthTable[0].plot(ylim=(0,100),ylabel="Deep Sleep (%)",xlim=(indexStartTime,indexEndTime)).grid(axis='y')
+fifthTable[1].plot(secondary_y=True, ax=plt.gca(), ylabel="Intensity")
 
 plt.show()
 
